3fdd5fee43c74510865b5e250839b98c8c49b2c0
Ricardo Carrillo <ricar60@mit.edu>
2025-08-21 22:15:19 -0400

    Torch Neural Net backbone

--- Neural_Network/XOR_sample.py
+++ Neural_Network/XOR_sample.py
@@ -1,4 +1,21 @@
 import torch
 
+### pyTorch Implementation ###
+class XOR_NN:
+    def __init__(self):
+        """
+        Initialize the Neural Network
+        """
 
-# To implement
+    def forward(self, x):
+        """
+        Forward feed through the network
+        """
+        pass
+
+    
+    def backward(self, y):
+        """
+        Back Propagation through the network
+        """
+        pass
